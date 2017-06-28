49966c1a332ef52b56fd20a099b4b7999425c511
qqss <1540973238@qq.com>
2017-06-28 10:03:08 +0800

    js change

--- insurance/static/common.js
+++ insurance/static/common.js
@@ -17,25 +17,3 @@
     win.addEventListener(resizeEvt, recalc, false);
     doc.addEventListener('DOMContentLoaded', recalc, false);
 })(document, window);
-$(function () {
-    //弹窗
-    $('#app_list li:not(:last)').on('click',function () {
-        var title =['仅限车险','仅限医疗理赔','不限险种','仅限车险','支持车险及寿险'];
-        var img_url = ['pop_01','pop_02','pop_03','pop_04','pop_05'];
-        var index = $(this).index();
-        $('#popup .child p').text(title[index]);
-        $("#i_type").attr("src","../static/img/"+img_url[index]+".jpg");
-        $('#popup').fadeIn(1000);
-    });
-    $('#pop_close').on('click',function () {
-        $('#popup').fadeOut();
-    })
-    //切换
-    $('.m-container .nav li').on('click',function () {
-        var index = $(this).index();
-        var width = $(this).width()+ 32;
-        $(this).addClass('active').siblings().removeClass('active');
-        $('.m-container .nav p').stop(false,true).animate({'left':index*width +'px'},300)
-        $('.m-container .content .con_list').eq(index).fadeIn(200).siblings().hide();
-    })
-});
